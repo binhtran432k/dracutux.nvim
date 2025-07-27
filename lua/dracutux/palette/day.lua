e84baa06dff2d538ff7683598fb68c0435069914
Binh Tran <binhtran432k@gmail.com>
2025-07-27 15:23:20 +0700

    fix(palette): fix day and soft

--- lua/dracutux/palette/day.lua
+++ lua/dracutux/palette/day.lua
@@ -1,22 +1,28 @@
+---@type dracutux.Palete
 return {
-	bg = "#F8F8F2",
-	fg = "#282A36",
-	selection = "#E2E5D9",
-	comment = "#B2C2F4",
-	orange = "#BF683C",
-	black = "#FFFFFD",
-	red = "#EF4545",
-	green = "#20BA4B",
-	yellow = "#91890A",
-	purple = "#8D63D9",
-	pink = "#CF4996",
-	cyan = "#3B89DD",
-	white = "#21222C",
-	bright_red = "#FF5555",
-	bright_green = "#20DA4B",
-	bright_yellow = "#B1A90A",
-	bright_blue = "#9D63E9",
-	bright_magenta = "#DF5996",
-	bright_cyan = "#3BA9ED",
-	bright_white = "#282A36",
+	base = "#eeeeff",
+	ui_base = "#ffffff",
+
+	text = "#222233",
+	subtle_text = "#777788",
+
+	selection = "#ddccff",
+	ui_selection = "#ccccdd",
+
+	comment = "#aabbee",
+
+	cyan = "#6699bb",
+	blue = "#4477cc",
+
+	green = "#33aa55",
+	teal = "#339977",
+
+	pink = "#cc66aa",
+
+	purple = "#7744bb",
+
+	red = "#ee6666",
+
+	yellow = "#aa9944",
+	orange = "#cc7755",
 }
